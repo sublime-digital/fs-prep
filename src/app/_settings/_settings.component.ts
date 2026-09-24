e4ac3fe14a3d9ab70c54908c0749dd3a99e62728
sds <sublimedigitalsolutions@gmail.com>
2026-09-25 00:35:58 +0600

    FAQ added to settings page

--- src/app/_settings/_settings.component.ts
+++ src/app/_settings/_settings.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-settings',
     templateUrl: './_settings.component.html',
-    styleUrls: ['./_settings.component.css'],
+    styleUrls: ['./_settings.component.scss'],
     standalone: false
 })
 export class SettingsComponent {
